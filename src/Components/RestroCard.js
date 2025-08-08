e1001c12e47c879bf23e431022aae1b88d5b413e
RAKESH40-lgtm <ram19042k@protonmail.com>
2025-08-08 11:42:49 +0530

    changed css

--- src/Components/RestroCard.js
+++ src/Components/RestroCard.js
@@ -4,15 +4,15 @@ const RestroCard = (props) => {
     const { name, avgRating, cuisines, sla, cloudinaryImageId } = props.RestoData.info
     return (
         <div className="w-[250px] m-6 p-5 bg-gray-100 hover:bg-gray-300">
-            <div className="p-3">
+            <div className="p-3 ">
                 <img src={`${IMAGE_URL}${cloudinaryImageId}`} />
-            </div>
-            <p className="text-2xl p-1">{name}</p>
+            <p className="text-1xl p-1">{name}</p>
             <p className="p-1">
                 <span>{avgRating} stars</span>
                 <span className="px-2">{sla.deliveryTime} mins</span>
             </p>
-            <p className="p-1 text-[14px] text-black">{cuisines.join(",")}</p>
+            <p className="p-1 text-[10px] text-black">{cuisines.join(",")}</p>
+            </div>
         </div>
     )
 }
@@ -21,7 +21,7 @@ export const withEnhancedRestroCard = (RestroCard) => {
         const {aggregatedDiscountInfoV3}=props?.RestoData?.info
         return (
             <>
-                <p className="relative top-68 left-30 text-white">{aggregatedDiscountInfoV3.header} <span>{aggregatedDiscountInfoV3.subHeader}</span></p>
+                <p className="relative top-68 left-15 text-white">{aggregatedDiscountInfoV3.header} <span>{aggregatedDiscountInfoV3.subHeader}</span></p>
                 <RestroCard {...props} />
             </>
         )
